0d38730ae16423ede7fbdce05d1b1adf095d23e9
Jeremiah <jermcdon18@gmail.com>
2025-11-25 16:52:27 -0600

    feat(ui): built Home screen layout with RecyclerView for the watchlist

--- app/src/main/java/com/example/pokemonpro/ui/HomeActivity.java
+++ app/src/main/java/com/example/pokemonpro/ui/HomeActivity.java
@@ -2,6 +2,8 @@ package com.example.pokemonpro.ui;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -9,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.pokemonpro.R;
 import com.example.pokemonpro.data.models.PokemonResponse;
@@ -21,12 +24,25 @@ import retrofit2.Response;
 public class HomeActivity extends AppCompatActivity {
 
     PokemonRepository repo = new PokemonRepository();
+    EditText etSearch;
+    Button btnAdd;
+    RecyclerView rv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        etSearch = findViewById(R.id.etSearch);
+        btnAdd = findViewById(R.id.btnAdd);
+        rv = findViewById(R.id.rvWatchlist);
+
+        // temp onClick
+        btnAdd.setOnClickListener(v -> {
+            String input = etSearch.getText().toString();
+            Toast.makeText(this, "Search: " + input, Toast.LENGTH_SHORT).show();
+        });
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
